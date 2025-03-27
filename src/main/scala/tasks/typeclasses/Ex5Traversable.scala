6c27acb61ee58e2794cd977d29a1fe7f0f41aeea
Matteo Susca <matteo.susca@studio.unibo.it>
2025-03-27 12:48:04 +0100

    traversable generic logAll

--- src/main/scala/tasks/typeclasses/Ex5Traversable.scala
+++ src/main/scala/tasks/typeclasses/Ex5Traversable.scala
@@ -1,6 +1,8 @@
 package u04lab
-import u03.Sequences.* 
+import u03.Sequences.*
 import Sequence.*
+import u03.Optionals.Optional
+import u03.Optionals.Optional.*
 
 /*  Exercise 5: 
  *  - Generalise by ad-hoc polymorphism logAll, such that:
@@ -19,8 +21,34 @@ object Ex5Traversable:
 
   def log[A](a: A): Unit = println("The next element is: "+a)
 
-  def logAll[A](seq: Sequence[A]): Unit = seq match
-    case Cons(h, t) => log(h); logAll(t)
-    case _ => ()
+  def logInLine[A](a: A): Unit = print(a.toString + ", ")
 
-  
+  trait Traversable[T[_]]:
+    def traverse[A](seq: T[A], f: A => Unit): Unit
+    extension [A](seq: T[A])
+      def logAll(f: A => Unit = log): Unit = traverse(seq, f)
+
+
+  given Traversable[Optional] with
+    def traverse[A](opt: Optional[A], f: A => Unit): Unit = opt match
+      case Just(a) => f(a)
+      case _ => ()
+
+  given Traversable[Sequence] with
+    def traverse[A](seq: Sequence[A], f: A => Unit): Unit = seq match
+      case Cons(h, t) => f(h); traverse(t, f)
+      case _ => ()
+
+  /* def logAll[A](seq: Sequence[A]): Unit = seq match
+    case Cons(h, t) => log(h); logAll(t)
+    case _ => () */
+
+  // main
+  @main def tryTraversable =
+    val si = Cons(10, Cons(20, Cons(30, Nil())))
+    si.logAll() // 10, 20, 30
+    si.logAll(logInLine)
+    println()
+
+    val opt = Optional.Just(4)
+    opt.logAll()
